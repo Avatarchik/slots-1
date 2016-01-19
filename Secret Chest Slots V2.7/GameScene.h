4b0aca59fd875407e5bb7586d433bf2a316fc2b8
Eddy Fan <eddy.fan@aofl.com>
2016-01-18 16:10:25 -0800

    added default GameScene.sks, .h, .m and GameViewController.m from an empty sprite kit project

---
+++ Secret Chest Slots V2.7/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  SpriteKitSlot
+//
+
+//  Copyright (c) 2016 Fantapstic Studio. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Secret Chest Slots V2.7/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SpriteKitSlot
+//
+
+//  Copyright (c) 2016 Fantapstic Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
