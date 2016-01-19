2374070f9744bc0292bd830219a358c9a4296de8
Eddy Fan <eddy.fan@aofl.com>
2016-01-19 15:07:46 -0800

    added nodename

---
+++ Secret Chest Slots V2.7/Template/Other FW/cfg/NodeNames.h
@@ -0,0 +1,12 @@
+//
+//  NodeNames.h
+//  Secret Chest Slots V2.7
+//
+//  Created by Eddy Fan on 1/19/16.
+//  Copyright © 2016 outlandishappsllc. All rights reserved.
+//
+
+#define kNodeLobby @"kNodeLobby";
+//#define kNodeLobby @"kNodeLobby";
+//#define kNodeLobby @"kNodeLobby";
+//#define kNodeLobby @"kNodeLobby";
