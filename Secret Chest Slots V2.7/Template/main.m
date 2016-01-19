e3eaa3a0ce93e0d98f7d962c3da92616d9ae4df0
Eddy Fan <eddy.fan@aofl.com>
2016-01-18 16:13:06 -0800

    updated main.m

--- Secret Chest Slots V2.7/Template/main.m
+++ Secret Chest Slots V2.7/Template/main.m
@@ -5,13 +5,18 @@
 //  Created by Slavian on 2013-08-17.
 //  Copyright bsixlux 2013. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
+#import "AppDelegate.h"
 
-int main(int argc, char *argv[]) {
-    
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
-    [pool release];
-    return retVal;
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
 }
+//int main(int argc, char *argv[]) {
+//    
+//    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+//    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+//    [pool release];
+//    return retVal;
+//}
